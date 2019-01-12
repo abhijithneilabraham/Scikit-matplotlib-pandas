4e26bf0e67c42b0e320c0faa96bacf080bf66e42
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-01-12 22:46:40 +0530

    I like the beginning

---
+++ untitled1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 12 22:05:10 2019
+
+@author: abhijithneilabraham
+"""
+
+import matplotlib.pyplot as plt
+x=[[2],[2.5],[1],[5],[8]]
+y=[[45],[58],[20],[65],[90]]
+plt.figure() #creates a new figure
+plt.title('marks obtained plotted against number of study hours')
+plt.xlabel('study hours')
+plt.ylabel('marks in 100')
+plt.plot(x,y,'k.')
