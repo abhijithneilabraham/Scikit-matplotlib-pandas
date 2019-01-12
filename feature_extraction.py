15a3b448451a25973cc859339899bab0d5183f76
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-01-13 00:34:37 +0530

    Update feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -15,7 +15,25 @@ corpus
 
 A collection of documents is called a corpus
 '''
-corpus={'I am programming right now','I love my life'}
+corpus={'I have the better laptop','I love my life'}
 from sklearn.feature_extraction.text import CountVectorizer
 vectorizer=CountVectorizer()
 print(vectorizer.fit_transform(corpus).todense())
+from nltk import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+import nltk
+nltk.download('wordnet')
+tags = ['n', 'v']#wordnet tags
+tok=[]
+for i in corpus:
+    tok.append(word_tokenize(i))
+print(tok)
+lem=WordNetLemmatizer()
+cut=[]
+for j in tok[0]:
+    print(j)
+    cut.append(lem.lemmatize(j,tags[0].lower()))
+print(cut)
+    
+        
+    
